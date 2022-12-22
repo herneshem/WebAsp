221da2d8756a6f6a8b8cf982301c0de6ccd80b00
herneshem <ernestoami@hotmail.com>
2022-12-22 01:32:39 +0100

    Agregar archivos de proyecto.

---
+++ WebAsp/Pages/Contenedor.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAsp.Pages
+{
+    public class ContenedorModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public ContenedorModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
